9e4622292c54e876e26d1add495ef9b8cc85b01f
David Honess <dave@raspberrypi.org>
2015-05-20 15:46:30 +0000

    pep8 tidy up

--- soyuz.py
+++ soyuz.py
@@ -9,7 +9,11 @@ display = pi3d.Display.create()
 cam = pi3d.Camera.instance()
 
 shader = pi3d.Shader("mat_light")
-model = pi3d.Model(file_string="apollo-soyuz.obj", name="model", x=0, y=-1, z=40, sx=1, sy=1, sz=1,)
+
+model = pi3d.Model(
+    file_string="apollo-soyuz.obj",
+    name="model", x=0, y=-1, z=40, sx=1, sy=1, sz=1)
+
 model.set_shader(shader)
 
 cam.position((0, 20, 0))
@@ -31,16 +35,16 @@ while display.loop_running():
     yaw = o["yaw"]
 
     yaw_total = yaw + math.radians(yaw_offset)
-        
+
     sin_y = math.sin(yaw_total)
     cos_y = math.cos(yaw_total)
-        
+
     sin_p = math.sin(pitch)
     cos_p = math.cos(pitch)
-        
+
     sin_r = math.sin(roll)
     cos_r = math.cos(roll)
-        
+
     abs_roll = math.degrees(math.asin(sin_p * cos_y + cos_p * sin_r * sin_y))
     abs_pitch = math.degrees(math.asin(sin_p * sin_y - cos_p * sin_r * cos_y))
 
@@ -48,7 +52,7 @@ while display.loop_running():
     model.rotateToX(abs_pitch)
     model.rotateToY(math.degrees(yaw_total))
     model.draw()
-        
+
     keypress = keyb.read()
 
     if keypress == 27:
